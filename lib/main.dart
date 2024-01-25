7fc60bd1ef5963fd3eea13bb08559b498ad65f50
anup <anuptwri007@gmail.com>
2024-01-25 19:34:46 +0545

    notification issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:swiftpages/ui/spashScreen.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -32,6 +32,9 @@ Future<void> main() async {
   // Variable to store the latest activity
   Map<String, dynamic>? latestActivity;
 
+  // Flag to check if it's the initial data retrieval
+  bool initialDataRetrieved = false;
+
   // Create a stream to listen for changes in the activity collection
   Stream<List<Map<String, dynamic>>> activitiesStream =
   FirebaseFirestore.instance
@@ -47,8 +50,6 @@ Future<void> main() async {
     return activities;
   });
 
-  // ... (your existing code)
-
   activitiesStream.listen((List<Map<String, dynamic>> activities) {
     if (activities.isNotEmpty) {
       // Sort activities based on timestamp in descending order
@@ -56,17 +57,29 @@ Future<void> main() async {
           (b['time'] as Timestamp).compareTo(a['time'] as Timestamp));
 
       // Get the latest activity
-      Map<String, dynamic> latestActivity = activities.first;
+      Map<String, dynamic> newLatestActivity = activities.first;
 
-      // Show the latest notification
-      showNotification(
-        flutterLocalNotificationsPlugin,
-        latestActivity['type'],
-        latestActivity['activityBy'],
-      );
+      // Check if it's the initial data retrieval
+      if (!initialDataRetrieved) {
+        // Update the latest activity without showing a notification
+        latestActivity = newLatestActivity;
+        initialDataRetrieved = true;
+      } else {
+        // Check if the new activity is different from the latest one
+        if (latestActivity == null || latestActivity != newLatestActivity) {
+          // Show the latest notification
+          showNotification(
+            flutterLocalNotificationsPlugin,
+            newLatestActivity['type'],
+            newLatestActivity['activityBy'],
+          );
+
+          // Update the latest activity
+          latestActivity = newLatestActivity;
+        }
+      }
     }
   });
-
 
   runApp(MyApp(
     flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin,
@@ -84,20 +97,17 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(
-
-      ),
+      home: SplashScreen(),
     );
   }
 }
 
-
-
 Future<void> showNotification(
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
     String type,
-    String activityBy) async {
-  log("here"+type + activityBy);
+    String activityBy,
+    ) async {
+  log("here" + type + activityBy);
   var androidPlatformChannelSpecifics = AndroidNotificationDetails(
     'notification_swift_pages',
     'Swift Pages',
